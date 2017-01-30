0fb5d5d0951c35ad92c5ba584468d3593966854f
ALISSABELLY\alissabelly <leo_lollipops@hotmail.com>
2017-01-30 20:35:58 +0700

    change route web for me

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +10,24 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/trainset_management', function () {
+    return view('trainset_management');
+});
+Route::get('/maintenance_plan', function () {
+    return view('maintenance_plan');
+});
+Route::get('/depot_management', function () {
+    return view('depot_management');
+});
+Route::get('/edit_trainset_management', function () {
+    return view('edit_trainset_management');
+});
+Route::get('/edit_maintenance_plan', function () {
+    return view('edit_maintenance_plan');
+});
+Route::get('/edit_depot_management', function () {
+    return view('edit_depot_management');
 });
